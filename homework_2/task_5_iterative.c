95f3c1e446046adace9e2103e554613bfa81eca8
mi-sts <mi.sts@yandex.ru>
2020-10-01 00:09:48 +0300

    Add solution for task 5

---
+++ homework_2/task_5_iterative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    long long number = 0;
+
+    printf("Введите число, факториал которого хотите найти: ");
+    scanf("%lld", &number);
+
+    while (number < 0) {
+        printf("Число должно быть неотрицательным!\n");
+        printf("Введите число, факториал которого хотите найти: ");
+        scanf("%lld",   &number);
+    }
+
+    long long numberFactorial = 1;
+    long long numberMultiplier = number;
+
+    while (numberMultiplier != 0) {
+        numberFactorial *= numberMultiplier;
+        numberMultiplier--;
+    }
+
+    printf("Факториал %lld равен %lld", number, numberFactorial);
+
+    return 0;
+}
---
+++ homework_2/task_5_recursive.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long findFactorial(long long n)
+{
+   return n > 1 ? findFactorial(n - 1) * n : 1;
+}
+
+int main()
+{
+    long long number = 0;
+
+    printf("Введите число, факториал которого хотите найти: ");
+    scanf("%lld", &number);
+
+    while (number < 0) {
+        printf("Число должно быть неотрицательным!\n");
+        printf("Введите число, факториал которого хотите найти: ");
+        scanf("%lld",   &number);
+    }
+
+    long long numberFactorial = findFactorial(number);
+    printf("Факториал %lld равен %lld", number, numberFactorial);
+
+    return 0;
+}
