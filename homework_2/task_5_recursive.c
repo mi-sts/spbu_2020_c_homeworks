46870d8db7caf914436587395315263c45167368
mi-sts <mi.sts@yandex.ru>
2020-10-01 00:23:10 +0300

    Update task_5_recursice.c

--- homework_2/task_5_recursive.c
+++ homework_2/task_5_recursive.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long findFactorial(long long n) {
-   return n > 1 ? findFactorial(n - 1) * n : 1;
+long long findFactorial(long long n)
+{
+    return n > 1 ? findFactorial(n - 1) * n : 1;
 }
 
 int main()
