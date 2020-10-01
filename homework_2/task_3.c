7897dfa8aeaba75db1317e23dcf77af0f465527f
mi-sts <mi.sts@yandex.ru>
2020-10-02 00:11:22 +0300

    Update task_3.c, CMakeLists.txt, numericOperations.c, numericOperations.h

--- homework_2/task_3.c
+++ homework_2/task_3.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../library/commonUtils/numericOperations.h"
 
-int main()
+// Функция ввода данных.
+int* getInputArray(int* n)
 {
-    int n = 0;
-
     printf("Введите длину массива: ");
-    scanf("%d", &n);
+    scanf("%d", n);
 
-    int* array = (int*)calloc(n, sizeof(int));
+    int* array = (int*)calloc(*n, sizeof(int));
 
     printf("Введите элементы массива:\n");
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < *n; ++i)
         scanf("%d", &array[i]);
 
-    int difference = 0; // Переменая, показывающая, сколько нулей встретилось при обходе массива(разница в индексе).
-    int temp = 0;
+    return array;
+}
 
-    for (int i = 0; i < n; ++i) {
+// Функция перемещающая нули в массиве в конец.
+void moveZeros(int* array, int arrayLength)
+{
+    int difference = 0; // Переменая, показывающая, сколько нулей встретилось при обходе массива(разница в индексе).
+
+    for (int i = 0; i < arrayLength; ++i) {
         if (array[i] == 0) {
             difference++;
             continue;
@@ -26,15 +31,28 @@ int main()
         if (difference == 0)
             continue;
 
-        temp = array[i - difference]; // Меняем местами текущий элемент и стоящий на different меньше по индексу элемент.
-        array[i - difference] = array[i];
-        array[i] = temp;
+        swap(&array[i - difference], &array[i]); // Меняем местами текущий элемент и стоящий на different меньше по индексу элемент.
     }
+}
 
+// Функция вывода массива.
+void printArray(int* array, int arrayLength)
+{
     printf("Массив с перемещёнными в конец нулями:\n");
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < arrayLength; i++)
         printf("%d ", array[i]);
+}
+
+int main()
+{
+    int n = 0;
+
+    int* array = getInputArray(&n);
+    moveZeros(array, n);
+    printArray(array, n);
+
+    free(array);
 
     return 0;
 }
