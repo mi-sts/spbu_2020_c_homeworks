344efa67a2f53b8b1fc81735c090c90db5dba86a
mi-sts <mi.sts@yandex.ru>
2020-11-10 13:09:21 +0300

    Resolve conflicts

--- test_2/task_2.c
+++ test_2/task_2.c
@@ -25,9 +25,9 @@ bool isDate(char* text)
             if (!isPoint(text[i]))
                 return false;
         }
-        else if (!isDigit(text[i])) {
+
+        else if (!isDigit(text[i]))
             return false;
-        }
     }
 
     return true;
